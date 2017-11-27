0f593b69b699e89241338d897ba057913536113a
Ntholi Nkhatho <ntholi@live.com>
2017-11-27 14:49:03 +0200

    Rename GenericServlet's methods

--- src/regonline/GenericServlet.java
+++ src/regonline/GenericServlet.java
@@ -7,8 +7,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public abstract class GenericServlet extends HttpServlet{
+import regonline.datasource.DAO;
+import regonline.faculty.Faculty;
+
+public abstract class GenericServlet<T> extends HttpServlet{
 	private static final long serialVersionUID = 1L;
+	protected DAO<T> dao;
+	
+	
+	public GenericServlet(Class<T> type){
+		dao = new DAO<>(type);
+	}
 	
 	/**
 	 * The method takes care of GET requests, if a request has a parameter named <b>d</b> 
@@ -17,7 +26,7 @@ public abstract class GenericServlet extends HttpServlet{
 	 * The <b>u</b>'s value should contain the id of a record that has to be deleted
 	 * If the method does not have the <b>d</b> parameter, the request will be treated as a normal 
 	 * GET request and the request will be handled by the
-	 * {@link #get(HttpServletRequest, HttpServletResponse)} method
+	 * {@link #all(HttpServletRequest, HttpServletResponse)} method
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
@@ -26,27 +35,27 @@ public abstract class GenericServlet extends HttpServlet{
 			delete(request, response, deleteId);
 		}
 		else{
-			get(request, response);
+			all(request, response);
 		}
 	}
 
 	/**
 	 * The method takes care of POST requests, if a request has a parameter named <b>u</b>
 	 * it will be treated as a PUT request and therefore the request will be handled by 
-	 * {@link #put(HttpServletRequest, HttpServletResponse, String)} method <br/>
+	 * {@link #update(HttpServletRequest, HttpServletResponse, String)} method <br/>
 	 * The <b>u</b>'s value should contain the id of a record that has to be updated
 	 * If the method does not have the <b>u</b> parameter, the request will be treated as a normal 
 	 * POST request and the request will be handled by the
-	 * {@link #post(HttpServletRequest, HttpServletResponse)} method
+	 * {@link #create(HttpServletRequest, HttpServletResponse)} method
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException{
 		String updateId = request.getParameter("u");
 		if(updateId != null){
-			put(request, response, updateId);
+			update(request, response, updateId);
 		}
 		else{
-			post(request, response);
+			create(request, response);
 		}
 	}
 	
@@ -58,7 +67,11 @@ public abstract class GenericServlet extends HttpServlet{
 	 * @throws ServletException
 	 * @throws IOException
 	 */
-	protected abstract void get(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+	protected void all(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException{
+		request.setAttribute("all", dao.all());
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
 	
 	/**
 	 * Handles POST requests
@@ -68,10 +81,11 @@ public abstract class GenericServlet extends HttpServlet{
 	 * @throws ServletException
 	 * @throws IOException
 	 */
-	protected abstract void post(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+	protected abstract void create(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
 	
 	/**
-	 * Handles PUT requests (used for updating a record), which are technically POST requests
+	 * Used for updating a record, a POST request has to have a <code>u</code> parameter to be treated as
+	 * an update, the  <code>u</code> parameter indicates the id of the resource to be updated
 	 * @see #doPost(HttpServletRequest, HttpServletResponse)
 	 * @param request
 	 * @param response
@@ -79,10 +93,11 @@ public abstract class GenericServlet extends HttpServlet{
 	 * @throws ServletException
 	 * @throws IOException
 	 */
-	protected abstract void put(HttpServletRequest request, HttpServletResponse response, String id) throws ServletException, IOException;
+	protected abstract void update(HttpServletRequest request, HttpServletResponse response, String id) throws ServletException, IOException;
 	
 	/**
-	 * Handles DELETE requests which are technically GET requests
+	 * Used for deleting a resource, this is called from a GET request that has a <code>d</code> parameter, which
+	 * is an id of the resource to be deleted
 	 * @see #doGet(HttpServletRequest, HttpServletResponse)
 	 * @param request
 	 * @param response
